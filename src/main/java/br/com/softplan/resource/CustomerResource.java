4e5b8bce24ff0e6e88127b79f81bf05d25b7a6e6
nathanbraga <braganathan38@gmail.com>
2024-07-13 03:04:32 -0300

    feat: Add log into resource

--- src/main/java/br/com/softplan/resource/CustomerResource.java
+++ src/main/java/br/com/softplan/resource/CustomerResource.java
@@ -10,10 +10,14 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("clientes")
 @ApplicationScoped
 public class CustomerResource {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(CustomerResource.class);
 
     @Inject
     TransactionService transactionService;
@@ -26,12 +30,14 @@ public class CustomerResource {
 
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createTransaction(@PathParam("id") Long id, @Valid @RequestBody TransactionRequest transactionRequest) {
+        LOGGER.info("Request received for createTransaction: id={}, body={}", id, transactionRequest);
         return Response.status(Response.Status.OK).entity(transactionService.createTransaction(id, transactionRequest)).build();
     }
 
     @GET
     @Path("{id}/extrato")
     public Response getExtract(@PathParam("id") Long id) {
+        LOGGER.info("Request received for getExtract: id={}", id);
         return Response.status(Response.Status.OK).entity(balanceService.getExtract(id)).build();
     }
 }
